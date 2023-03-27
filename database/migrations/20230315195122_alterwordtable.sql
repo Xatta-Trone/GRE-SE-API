1042e86f6674bcada805bd9fd68bf86a2f79469c
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2023-03-27 22:07:07 +0900

    added user table

--- database/migrations/20230315195122_alterwordtable.sql
+++ database/migrations/20230315195122_alterwordtable.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
-
-ALTER TABLE `wordlist` MODIFY `word` VARCHAR(255) NOT NULL;
-
+ALTER TABLE
+    `wordlist`
+MODIFY
+    `word` VARCHAR(255) NOT NULL;
 
 -- +goose StatementEnd
-
 -- +goose Down
 -- +goose StatementBegin
 -- SELECT 'down SQL query';
---
+++ database/migrations/20230327215324_add_users_table.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `username` VARCHAR(255) NOT NULL UNIQUE,
+    `created_at` DATETIME NULL,
+    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY `pk_id`(`id`)
+) ENGINE = InnoDB;
+
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE users;
+
+-- +goose StatementEnd
