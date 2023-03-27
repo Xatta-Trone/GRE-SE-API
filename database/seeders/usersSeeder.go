997731f11bba179c9767eee2bbeac28971af826c
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2023-03-27 23:03:06 +0900

    modified for batch import

--- database/seeders/usersSeeder.go
+++ database/seeders/usersSeeder.go
@@ -2,6 +2,7 @@ package seeders
 
 import (
 	"fmt"
+	"math"
 	"time"
 
 	"github.com/fatih/color"
@@ -12,41 +13,48 @@ import (
 )
 
 func UserSeed(db *sqlx.DB) {
-	const Total = 1000
+	const Total = 10000
 	color.Yellow("=== Inside the users seeder ===")
 	color.Yellow(fmt.Sprintf("=== Seeding %d users ===", Total))
 
-	users := []model.UserModel{}
+	// batch import per 1000 records
+	totalRun := int(math.Ceil(Total / 1000))
 
-	for i := 1; i <= Total; i++ {
-		user := model.UserModel{
-			Name:  faker.Name(),
-			Email: faker.Email(options.WithGenerateUniqueValues(true)),
-			// Email:    fmt.Sprintf("test-email%d@example.com", i),
-			UserName:  faker.Username(options.WithGenerateUniqueValues(true)),
-			CreatedAt: time.Now().UTC().Local(),
+	for i := 0; i < totalRun; i++ {
+
+		users := []model.UserModel{}
+
+		for i := 1; i <= 1000; i++ {
+			user := model.UserModel{
+				Name:  faker.Name(),
+				Email: faker.Email(options.WithGenerateUniqueValues(true)),
+				// Email:    fmt.Sprintf("test-email%d@example.com", i),
+				UserName:  faker.Username(options.WithGenerateUniqueValues(true)),
+				CreatedAt: time.Now().UTC().Local(),
+			}
+
+			// fmt.Println(user)
+
+			users = append(users, user)
+
 		}
 
-		// fmt.Println(user)
+		query := `INSERT Ignore INTO users (name,email,username,created_at) VALUES (:name,:email,:username,:created_at)`
+		res, err := db.NamedExec(query, users)
+		if err != nil {
+			fmt.Println("err", err)
+		}
 
-		users = append(users, user)
+		totalImported, err := res.RowsAffected()
+
+		if err != nil {
+			fmt.Println("err", err)
+		}
+
+		color.Green(fmt.Sprintf("Imported users: %d", int(totalImported)))
 
 	}
 
-	query := `INSERT Ignore INTO users (name,email,username,created_at) VALUES (:name,:email,:username,:created_at)`
-	res, err := db.NamedExec(query, users)
-	if err != nil {
-		fmt.Println("err", err)
-	}
-
-	totalImported, err := res.RowsAffected()
-
-	if err != nil {
-		fmt.Println("err", err)
-	}
-
-	color.Green(fmt.Sprintf("Total imported users: %d", int(totalImported)))
-
-	// fmt.Println(users)
+	color.Green(fmt.Sprintf("Total Imported users: %d", int(Total)))
 
 }
